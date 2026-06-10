04c693cdd6fe4d2d4dbe7e6d0faa993ab5f0c9f3
mustafa ashraf <mustafa.ashraf@yahoo.com>
2026-06-10 18:36:52 +0300

    Initial commit

---
+++ backend/src/test/java/com/dataScience/dataScience/DataScienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataScience.dataScience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataScienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
