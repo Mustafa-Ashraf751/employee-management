bb6b5008423e1d808e5606250ad9f61c461aa132
mustafa ashraf <mustafa.ashraf@yahoo.com>
2026-06-13 16:41:40 +0300

    fix: Change ID generation strategy from AUTO to IDENTITY in Department entity

--- backend/src/main/java/com/datascience/datascience/entity/Department.java
+++ backend/src/main/java/com/datascience/datascience/entity/Department.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Department {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false,unique=true)
